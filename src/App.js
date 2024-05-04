8ce4afdaedface822d948e74452fb4c54db52c0d
Jayshree Keskar <jayshreekeskkar321@gmail.com>
2024-05-04 20:35:05 +0530

    Fixed ES lint issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, Link, useNavigate} from "react-router-dom"
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
 import Home from "./pages/Home"
 import CreatePost from "./pages/CreatePost";
 import Login from "./pages/Login";
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
-import React , {useState} from "react";
-import {useEffect} from "react";
+import React , {useState, useEffect} from "react";
 import {collection, getDocs, deleteDoc, doc} from 'firebase/firestore'
 import {db, auth} from "../firebase-config";
 
